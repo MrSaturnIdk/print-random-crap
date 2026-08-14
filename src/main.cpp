37320af788a7a29d2e0ec75f16dc2b54f502c568
MrSaturnIdk <abrahm3314@gmail.com>
2026-08-14 22:44:49 +0000

    Add arguments

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
  */
 
 #include <cstdio>
+#include <map>
 #include <random>
+#include <string>
 
 int getRandomInt(int min, int max) {
     static std::random_device rd {};
@@ -25,7 +27,48 @@ char getRandomAsciiChar() {
     }
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    std::map<std::string, int> correlatedNumbers {
+        /// Misc
+        {"--help", 1},
+        {"--version", 2}
+    };
+    for (int i = 1; i < argc; i++) {
+        int num {correlatedNumbers[argv[i]]};
+        switch (num) {
+            case 1:
+                std::printf("%s%s%s\n%s\n%s\n\n%s\n%s\n%s\n%s\n",
+                    "Usage: ",
+                    argv[0],
+                    " [arguments]",
+                    "Infinitely print random letters.",
+                    "No arguments will print letters.",
+                    "List of arguments:",
+                    "Miscellaneous",
+                    "  --help        Prints this screen.",
+                    "  --version     Prints version."
+                );
+                return 0;
+            case 2:
+                std::printf("%s\n\n%s\n%s\n",
+                    "Print Random Crap 1.0.0",
+                    "Copyright © 2026 MrSaturnIdk",
+                    "Licensed under the AGPL-3.0-or-later"
+                );
+                return 0;
+            default:
+                std::printf("%s%s%s\n%s%s%s\n",
+                    "Unrecognized flag \"",
+                    argv[i],
+                    "\".",
+                    "See \"",
+                    argv[0],
+                    " --help\" for details."
+                );
+                return -1;
+        }
+    }
+
     for (;;) {
         std::printf("%c", getRandomAsciiChar());
         std::fflush(stdout);
