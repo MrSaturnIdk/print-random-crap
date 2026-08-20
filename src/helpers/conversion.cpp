e0741f4074978ebd06c31d0586d832db48e961ac
MrSaturnIdk <abrahm3314@gmail.com>
2026-08-20 22:09:03 +0000

    refactor: remove redundant static_cast
    Switch statements accept chars anyway...

--- src/helpers/conversion.cpp
+++ src/helpers/conversion.cpp
@@ -13,25 +13,24 @@ namespace helpers::conversion {
 
 bool isStdStringValidUnsignedInt(std::string string) {
     for (char c : string) {
-        // Characters 0-9 use 48-57 in ASCII, so getting the static_casted to int version minus 48 will get you 0-9
-        switch (static_cast<int> (c - 48)) {
-            case 1:
+        switch (c) {
+            case '1':
                 break;
-            case 2:
+            case '2':
                 break;
-            case 3:
+            case '3':
                 break;
-            case 4:
+            case '4':
                 break;
-            case 5:
+            case '5':
                 break;
-            case 6:
+            case '6':
                 break;
-            case 7:
+            case '7':
                 break;
-            case 8:
+            case '8':
                 break;
-            case 9:
+            case '9':
                 break;
             default:
                 return false;
