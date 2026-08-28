b1468f77d38875d30c5c133af69099417f7591df
MrSaturnIdk <abrahm3314@gmail.com>
2026-08-28 22:33:13 +0000

    fix!: allow using flag names as parameters
    BREAKING CHANGE: Trying to pass a flag to an arg will cause it to take
    it as the parameter over registering the flag as one.
    
    Adheres to POSIX standards for required argument parsing.

--- src/main.cpp
+++ src/main.cpp
@@ -27,86 +27,86 @@ int main(int argc, char* argv[]) {
         {"randomSeed", 1}
     };
     for (int i {1}; i < argc; ++i) {
-        int lookup {flagLookupTable[argv[i]]};
-        switch (lookup) {
-            /// Same order as in flagLookupTable
-            case 1: {
-                expectedFlagParameter = "randomSeed";
-                break;
-            }
-            case 2: {
-                if (helpers::random::onlyWhitespace) {
-                    helpers::random::onlyWhitespace = false;
+        // Expecting parameter
+        if (flagParameterLookupTable[expectedFlagParameter] == 0) {
+            int lookup {flagLookupTable[argv[i]]};
+            switch (lookup) {
+                /// Same order as in flagLookupTable
+                case 1: {
+                    expectedFlagParameter = "randomSeed";
+                    break;
                 }
-                helpers::random::noWhitespace = true;
-                break;
-            }
-            case 3: {
-                if (helpers::random::noWhitespace) {
-                    helpers::random::noWhitespace = false;
+                case 2: {
+                    if (helpers::random::onlyWhitespace) {
+                        helpers::random::onlyWhitespace = false;
+                    }
+                    helpers::random::noWhitespace = true;
+                    break;
                 }
-                helpers::random::onlyWhitespace = true;
-                break;
+                case 3: {
+                    if (helpers::random::noWhitespace) {
+                        helpers::random::noWhitespace = false;
+                    }
+                    helpers::random::onlyWhitespace = true;
+                    break;
+                }
+    
+                case 4: {
+                    std::printf("%s%s%s",
+                        "Usage: ", argv[0], " [arguments]\n"
+                        "Infinitely print random letters.\n"
+                        "No arguments will print letters.\n\n"
+                        "List of arguments:\n"
+                        "Modifies printing\n"
+                        "  --set-seed <seed>  Set random seed to <seed>.\n"
+                        "  --no-whitespace    Does not print spaces, newlines or tabs.\n"
+                        "  --only-whitespace  Only prints newlines.\n"
+                        "Miscellaneous\n"
+                        "  --help             Prints this screen.\n"
+                        "  --version          Prints version.\n"
+                    );
+                    return 0;
+                }
+                case 5: {
+                    std::printf("%s",
+                        "Print Random Crap " PROJECT_VER "\n\n"
+                        "Copyright © 2026 MrSaturnIdk.\n"
+                        "Licensed under the AGPL-3.0-or-later.\n\n"
+                        "Source code hosted at <" HOMEPAGE ">.\n"
+                    );
+                    return 0;
+                }
+                default: {
+                    std::fprintf(stderr, "%s%s%s%s%s",
+                        "\033[1;31mError:\033[0m Unrecognized flag \"", argv[i], "\".\n"
+                        "See \"", argv[0], " --help\" for details.\n"
+                    );
+                    return 1;
+                }
             }
-
-            case 4: {
-                std::printf("%s%s%s",
-                    "Usage: ", argv[0], " [arguments]\n"
-                    "Infinitely print random letters.\n"
-                    "No arguments will print letters.\n\n"
-                    "List of arguments:\n"
-                    "Modifies printing\n"
-                    "  --set-seed <seed>  Set random seed to <seed>.\n"
-                    "  --no-whitespace    Does not print spaces, newlines or tabs.\n"
-                    "  --only-whitespace  Only prints newlines.\n"
-                    "Miscellaneous\n"
-                    "  --help             Prints this screen.\n"
-                    "  --version          Prints version.\n"
-                );
-                return 0;
-            }
-            case 5: {
-                std::printf("%s",
-                    "Print Random Crap " PROJECT_VER "\n\n"
-                    "Copyright © 2026 MrSaturnIdk.\n"
-                    "Licensed under the AGPL-3.0-or-later.\n\n"
-                    "Source code hosted at <" HOMEPAGE ">.\n"
-                );
-                return 0;
-            }
-            default: {
-                int lookup {flagParameterLookupTable[expectedFlagParameter]};
-                switch (lookup) {
-                    /// In the same order as flagParameterLookupTable, similar to flagLookupTable
-                    case 1: {
-                        std::uint32_t seed {};
-                        std::string string {argv[i]};
-                        if (!helpers::conversion::isStdStringValidUnsignedInt(string)) {
-                            std::string tempString {};
-                            for (char c : string) {
-                                tempString += c;
-                            }
-                            string = tempString;
+        } else {
+            int lookup {flagParameterLookupTable[expectedFlagParameter]};
+            switch (lookup) {
+                /// In the same order as flagParameterLookupTable, similar to flagLookupTable
+                case 1: {
+                    std::uint32_t seed {};
+                    std::string string {argv[i]};
+                    if (!helpers::conversion::isStdStringValidUnsignedInt(string)) {
+                        std::string tempString {};
+                        for (char c : string) {
+                            tempString += c;
                         }
-                        seed = helpers::conversion::stdStringToInt(string);
-                        helpers::random::randomSeed = seed;
-                        helpers::random::usingCustomSeed = true;
-                        expectedFlagParameter = "";
-                        break;
+                        string = tempString;
                     }
-                    default: {
-                        std::fprintf(stderr, "%s%s%s%s%s",
-                            "\033[1;31mError:\033[0m Unrecognized flag \"", argv[i], "\".\n"
-                            "See \"", argv[0], " --help\" for details.\n"
-                        );
-                        return 1;
-                    }
+                    seed = helpers::conversion::stdStringToInt(string);
+                    helpers::random::randomSeed = seed;
+                    helpers::random::usingCustomSeed = true;
+                    expectedFlagParameter = "";
+                    break;
                 }
-                break;
             }
         }
     }
-
     if (expectedFlagParameter != "") {
         std::fprintf(stderr, "\033[1;31mError:\033[0m Parameter for argument \"%s\" not given.\n", argv[argc - 1]);
         return 1;
