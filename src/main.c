6454d342ebf3dc22b2a0994df9a9e8f89456b289
MrSaturnIdk <abrahm3314@gmail.com>
2026-09-06 21:04:50 +0000

    refactor: remove "last maintained version"
    I might decide to touch this again

--- src/main.c
+++ src/main.c
@@ -64,7 +64,7 @@ int main(int argc, char* argv[]) {
             }
             case 3: {
                 printf("%s",
-                    "Print Random Crap " PROJECT_VER " (last maintained version)\n\n"
+                    "Print Random Crap " PROJECT_VER "\n\n"
                     "Copyright © 2026 MrSaturnIdk.\n"
                     "Licensed under the AGPL-3.0-or-later.\n\n"
                     "Source code hosted at <" HOMEPAGE ">.\n"
